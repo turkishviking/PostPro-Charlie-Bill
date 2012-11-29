972d8759fa9b77d35e658086862c4cd7192eaa71
turkishviking <c.lutaud@hotmail.fr>
2012-11-29 14:32:18 +0100

    Netoyage OGL
    c plus propre!

--- OGL.py
+++ OGL.py
@@ -14,155 +14,98 @@ from PyQt4 import QtCore, QtGui
 from decimal import Decimal
 
 class GLWidget(QtOpenGL.QGLWidget):
-    global liste
-    global listeMinMax
-    global mode
-    global xx1
-    global yy1
-    global AngleX
-    global AngleY
-    global AngleZ
-    global angle
-    
+
     def wheelEvent(self, event):
         super(GLWidget, self).wheelEvent(event)
-        global angle
-        global h
-        global w
-        angle = angle +(event.delta()/12)
-        self.resizeGL(w, h)
+        self.angle = self.angle +(event.delta()/12)
+        self.resizeGL(self.w, self.h)
         self.updateGL()
         
     def mousePressEvent(self, mouseEvent):
-        global mode
         if mouseEvent.button() == QtCore.Qt.LeftButton :
-            mode = 1
+            self.mode = 1
             
     def mouseReleaseEvent(self, e):
-        global mode
-        mode =0
+        self.mode =0
         
-  
     def mouseMoveEvent(self, event):
-        global xx1
-        global yy1
-        global mode
-        global AngleX
-        global AngleY
-        global AngleZ
-        if mode == 1:
+        if self.mode == 1:
             xx = event.pos().x()
             yy = event.pos().y()
             try:
-                AngleX = AngleX + xx - xx1
-                AngleY = AngleY + yy - yy1
-            except (UnboundLocalError, NameError):
+                self.AngleX = self.AngleX + xx - self.xx1
+                self.AngleY = self.AngleY + yy - self.yy1
+            except (UnboundLocalError, NameError,  AttributeError):
                 pass
            
-            xx1 = xx
-            yy1 = yy
+            self.xx1 = xx
+            self.yy1 = yy
             self.updateGL()
             
     
     def ajoute(self, li, listeCoo):
-        global liste
-        global listeMinMax 
-        listeMinMax = listeCoo
-        liste = li
+        self.listeMinMax = listeCoo
+        self.liste = li
         
     
     def __init__(self, parent=None):
-        global mode
-        global AngleX
-        global AngleY
-        global AngleZ
-        global angle
-        angle = 70
-        AngleX=0
-        AngleY=0
-        AngleZ=0
+        self.angle = 70
+        self.AngleX=0
+        self.AngleY=0
+        self.AngleZ=0
         self.parent = parent
         QtOpenGL.QGLWidget.__init__(self, parent)
         self.yRotDeg = 0.0
         self.setMouseTracking(True)
-        mode = 0
+        self.mode = 0
         
 
     def initializeGL(self):
         self.qglClearColor(QtGui.QColor(0, 0,  0))
-  
-        
         glEnable(GL_DEPTH_TEST)
 
     def resizeGL(self, width, height):
-        global listeMinMax
-        global x
-        global y
-        global z
-        global aspect
-        global angle
-        global w
-        global h
-
-        w = width
-        h = height
+        self.w = width
+        self.h = height
         if height == 0: height = 1
 
         glViewport(0, 0, width, height)
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        aspect = width / float(height)
+        self.aspect = width / float(height)
         #---------------------------------CAMERA-----------------------------------#
-        GLU.gluPerspective(angle, aspect, 1.0, 1000.0)
-        
-        x = listeMinMax[3] + (listeMinMax[0]-listeMinMax[3] )/2
-        y = listeMinMax[4] + (listeMinMax[1]-listeMinMax[4] )/2
-        z = listeMinMax[5] + (listeMinMax[2]-listeMinMax[5] )/2
-        xmax= (listeMinMax[0]-listeMinMax[3] )*Decimal(str(1.5))
-        ymax= (listeMinMax[1]-listeMinMax[4] )*Decimal(str(1.5))
-        zmax= (listeMinMax[2]-listeMinMax[5] )*Decimal(str(1.5))
+        GLU.gluPerspective(self.angle, self.aspect, 1.0, 1000.0)
+        self.x = self.listeMinMax[3] + (self.listeMinMax[0]-self.listeMinMax[3] )/2
+        self.y = self.listeMinMax[4] + (self.listeMinMax[1]-self.listeMinMax[4] )/2
+        self.z = self.listeMinMax[5] + (self.listeMinMax[2]-self.listeMinMax[5] )/2
+        xmax= (self.listeMinMax[0]-self.listeMinMax[3] )*Decimal(str(1.5))
+        ymax= (self.listeMinMax[1]-self.listeMinMax[4] )*Decimal(str(1.5))
+        zmax= (self.listeMinMax[2]-self.listeMinMax[5] )*Decimal(str(1.5))
       
-        gluLookAt(xmax,ymax,zmax,x,y,z,0,0,1)
+        gluLookAt(xmax,ymax,zmax,self.x,self.y,self.z,0,0,1)
         glMatrixMode(GL_MODELVIEW)
 
     def paintGL(self):
-        global liste
-        global x
-        global y
-        global z
-        global AngleX
-        global AngleY
-        global AngleZ
-        global angle
-        global aspect
-        
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-       
-
         glLoadIdentity()
-        
-
-        glTranslated(x, y, z)
-        glRotatef(AngleX/3, 1.0, 0.0, 0.0)
-        glRotatef(AngleY/3, 0.0, 1.0, 0.0)
-        glTranslated(-x, -y, -z)
+        glTranslated(self.x, self.y, self.z)
+        glRotatef(self.AngleX/3, 1.0, 0.0, 0.0)
+        glRotatef(self.AngleY/3, 0.0, 1.0, 0.0)
+        glTranslated(-self.x, -self.y, -self.z)
         glPointSize (2.0)
 
-    
         # Clear buffers
         glClear(GL_COLOR_BUFFER_BIT)
 
         # Draw some stuff
         glBegin(GL_LINE_STRIP)
-        for ligne in liste:
+        for ligne in self.liste:
                 x1 =ligne[0]
                 y1 = ligne[1]
                 z1 = ligne[2]
                 glVertex3f(x1, y1, z1)
 
         glEnd()
-        
-
 
     def initGeometry(self):
         self.cubeVtxArray = array(
@@ -214,10 +157,3 @@ class MonDialog(object):
 
         self.gl.setSizePolicy(sizePolicy)
 
-
-
-
-
-
-
-        
